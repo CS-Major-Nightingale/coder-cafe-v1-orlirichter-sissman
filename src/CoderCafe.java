d73fccbb226ec7032239bc14877cf88f718fbd8e
orlirichtersissman <orichtersissman@nightingale.org>
2025-10-03 15:00:00 -0400

    new edits

--- src/CoderCafe.java
+++ src/CoderCafe.java
@@ -22,13 +22,13 @@ public class CoderCafe {
         //variable for total (all total drink costs added together)
         double subTotal = hotChocolateTotal+ matchaTotal+ cappuccinoTotal+ latteTotal;
         //variable for sales tax (the total times 8.875%)
-        double salesTax = subTotal *.08875;
+        double salesTax = (int)((subTotal *.08875)*100)/100.0;
         //variable for service fee which is always the same
         double serviceFee = 1.21;
         //variable for total number of items
         int numberItems = matcha + latte + cappuccino + hotChocolate;
         //variable for coder cafe fee(subtotal divided by the total number of items and then rounded down to the nearest center)
-        double coderCafeFee = (subTotal/numberItems)*((100)/100.0);
+        double coderCafeFee = (int)((subTotal/numberItems)*100)/100.0;
         //variable for total (everything added together)
         double total = subTotal + coderCafeFee + serviceFee + salesTax;
 
@@ -45,29 +45,29 @@ System.out.println("*\t\t " +formattedDate +"\t\t*");
 
         System.out.println("*\t\t\t\t\t\t\t\t\t*");
 // list the number of each item and the price
-        System.out.printf("* " + matcha + " X Iced Matcha" + "\t\t\t$ %.2f", matchaTotal);
-        System.out.println("  *");
-        System.out.printf( "* "+ latte + " X Iced Latte" + "\t\t\t$ %.2f",  latteTotal );
-        System.out.println(" *");
-        System.out.printf("* "+ cappuccino + " X Cappuccino" + "\t\t\t$ %.2f",  cappuccinoTotal );
-        System.out.println(" *");
-        System.out.printf("* "+ hotChocolate + " X Hot Chocolate" +  "\t\t\t$ %.2f", hotChocolateTotal);
-        System.out.println("  *");
+        System.out.printf("* " + matcha + " X Iced Matcha" + "\t\t\t$ %.2f  *", matchaTotal);
+        System.out.println();
+        System.out.printf( "* "+ latte + " X Iced Latte" + "\t\t\t$ %.2f  *",  latteTotal );
+        System.out.println();
+        System.out.printf("* "+ cappuccino + " X Cappuccino" + "\t\t\t$ %.2f  *",  cappuccinoTotal );
+        System.out.println();
+        System.out.printf("* "+ hotChocolate + " X Hot Chocolate" +  "\t\t\t$ %.2f  *", hotChocolateTotal);
+        System.out.println();
 
         //this is used for spacing with the border
         System.out.println("*\t\t\t\t\t\t\t\t\t*");
         //this is for all the calculations with the prices
-        System.out.printf("* Subtotal:" + "\t\t\t\t\t$ %.2f", subTotal);
-        System.out.println(" *");
-        System.out.printf("* Sales tax:" + ("\t\t\t\t$ %.2f"), salesTax) ;
-        System.out.println("  *");
-        System.out.printf("* Service Fee:" + ("\t\t\t\t$") + serviceFee);
-        System.out.println("   *");
-        System.out.printf("* Coder Cafe Fee:" + "\t\t\t$%.2f", coderCafeFee);
-        System.out.println("   *");
+        System.out.printf("* Subtotal:" + "\t\t\t\t\t$ %.2f *", subTotal);
+        System.out.println();
+        System.out.printf("* Sales tax:" + ("\t\t\t\t$ %.2f  *"), salesTax) ;
+        System.out.println();
+        System.out.printf("* Service Fee:" + ("\t\t\t\t$ ") + serviceFee + "  *");
+        System.out.println();
+        System.out.printf("* Coder Cafe Fee:" + "\t\t\t$ %.2f  *", coderCafeFee);
+        System.out.println();
         System.out.println("*\t\t\t\t\t\t\t\t\t*");
-        System.out.printf("* Total: " + "\t\t\t\t\t$%.2f", total);
-        System.out.println("  *");
+        System.out.printf("* Total: " + "\t\t\t\t\t$%.2f  *", total);
+        System.out.println();
         //closing message
         System.out.println("*\t\t Have a great day :)\t\t*");
         //rest of the border
